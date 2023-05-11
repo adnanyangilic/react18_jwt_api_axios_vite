3d55075d390efca63c21648be3ec7dbee9579940
adnanyangilic <65245974+adnanyangilic@users.noreply.github.com>
2023-05-11 14:42:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import Example from './Example'
+import Create from './Create'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
